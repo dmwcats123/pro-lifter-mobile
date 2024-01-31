594832f5f699b1bf62f60bb06874c865fd4d27a4
dmwcats <dmwcats123@gmail.com>
2024-01-31 12:48:50 -0600

    awsness

--- pro-lifter-mobile-backend/index.js
+++ pro-lifter-mobile-backend/index.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 const cors = require("cors");
 require("dotenv").config();
 
